1d4016e048f9c0135293810c84b14890320e9da5
Andrés Camperos <joseandrescram1005@gmail.com>
2025-12-04 22:53:40 -0500

    initial commit: add data analysis script and requirements
    - Created main.py to analyze student performance data from a CSV file.
    - Calculated average scores in math, reading, and writing.
    - Analyzed the relationship between parental education levels and average scores.
    - Computed average scores by gender.
    - Added requirements.txt to include necessary libraries: pandas, seaborn, and matplotlib.

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# read csv file
+df = pd.read_csv("datasets/StudentsPerformance.csv")
+
+# average score in each field
+average_score = df[["math score", "reading score", "writing score"]].mean()
+
+# relation of parental level education and avg score
+df["avg_score"] = df[["math score", "reading score", "writing score"]].mean(axis=1)
+df_ple_avg = df.groupby("parental level of education")["avg_score"].mean()
+print(f"========== PARENTAL EDUCATION / AVG SCORE ==========\n{df_ple_avg}")
+
+# average score per gender
+df_avg_gender = df.groupby("gender")["avg_score"].mean() 
+print(f"========== AVG PER GENDER ==========\n{df_avg_gender}")
